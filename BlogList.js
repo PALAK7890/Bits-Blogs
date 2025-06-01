319d5788c980e56a456b4619fb4a6379e3a36fce
Palak <palak@Palaks-MacBook-Pro.local>
2025-06-01 11:09:07 +0530

    restarting the project

--- BlogList.js
+++
@@ -1,8 +0,0 @@
-const BlogList = () => {
-    return (
-      <div className="blog-list">
-      </div>
-    );
-  }
-   
-  export default BlogList;
--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
-import Navbar from './Navbar';
-import SignInPage from './Login';
-import Home from './Home';
-import About from './About';
-import LogInPage from './Login';
-import CreateBlog from './createBlog';
+import React from 'react'
+import Navbar from './NavBar';
+const App = () => {
+    return ( 
+    <div className='container'>
+        <Navbar/>
+        
 
-function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
-  return (
-    <>
-
-      <Navbar onSignInClick={() => setCurrentPage('signin')} />
-        <CreateBlog/>
-      
-      {currentPage === 'signin' ? (
-        <SignInPage />
-      ) : (
-        <Home />
-      )}
-     
-    </>
-  );
+    </div>
+     );
 }
-
-export default App;
+ 
+export default App
